077934e80fc76d1e89adb00b5747ef8d30517616
SUBROTOKOIRY <kumarsubrato1103@gmail.com>
2023-04-20 01:34:50 +0530

    personal website

---
+++ script.js
@@ -0,0 +1,33 @@
+let controlbtn=document.querySelectorAll('.control')
+let activecontrolbtn=document.querySelector('.active-btn')
+let main=document.querySelector('.main')
+
+// adding bg-color on click and removing the previous one
+for(let i=0;i<controlbtn.length;i++)
+{   
+    controlbtn[i].addEventListener('click' ,function(){
+
+        activecontrolbtn.classList.remove('active-btn')
+        controlbtn[i].classList.add('active-btn');
+        activecontrolbtn=controlbtn[i];
+    })
+}
+//toggle on double click, the background color will disappear
+for(let i=0;i<controlbtn.length;i++)
+{
+    controlbtn[i].addEventListener('dblclick',function(){
+        controlbtn[i].classList.toggle('active-btn')
+    })
+}
+
+controlbtn.forEach(btn =>{
+    btn.addEventListener('click',(e)=>{
+        console.log(e.target)
+        document.querySelectorAll('.active').forEach(activememb =>{
+            activememb.classList.remove('active')
+        })  
+        btn.classList.add('active');
+        console.log(btn.dataset.id)
+        document.getElementById(btn.dataset.id).classList.add("active");
+    })
+})
